6fcf38cc962f3da3fb8da4e93a53c3364bbe545b
H. Jin <hjin25@uic.edu>
2020-02-23 20:01:50 -0600

    clean base class

--- models/base.py
+++ models/base.py
@@ -1,21 +1,4 @@
 from absl import flags
-# from models.layers import GraphConv
-# from models.utils import sp_matrix_to_sp_tensor
-# from sklearn.metrics import accuracy_score
-# from time import time
-# import numpy as np
-# import os
-# import scipy.sparse as sp
-# import tensorflow as tf
-# import warnings
-
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-# warnings.simplefilter(action='ignore', category=FutureWarning)
-# warnings.simplefilter(action='ignore', category=DeprecationWarning)
-
-# spdot = tf.sparse.sparse_dense_matmul
-# dot = tf.matmul
-
 FLAGS = flags.FLAGS
 
 
